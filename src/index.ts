d3db43307cbd84a44cec2ecc1c6d0b3cf50ca475
Nooc <nooc@nooc.me>
2025-04-24 20:20:08 +0800

    Complete the basic functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-import { Hono } from 'hono'
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
 
-const app = new Hono()
+const webhookUrl = process.env.WEBHOOK_URL;
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+if (!webhookUrl) {
+  throw Error("WEBHOOK_URL is not defined");
+}
 
-export default app
+// Create an MCP server
+const server = new McpServer({
+  name: "WebhookMCP",
+  version: "0.0.1",
+});
+
+// Add an addition tool
+server.tool("call-webhook", {}, async ({}) => {
+  const webhookUrl = process.env.WEBHOOK_URL;
+
+  await fetch(webhookUrl!);
+
+  return {
+    content: [],
+  };
+});
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
